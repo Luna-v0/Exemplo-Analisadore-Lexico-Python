746cfc2c63766e29071c07a13aa1d329c46fc588
Eduardo Dantas Luna <edudaluna2000@gmail.com>
2025-06-03 21:47:27 -0300

    added a simple sly example instead of the jupyter notebook

--- sly/main.py
+++ sly/main.py
@@ -1,24 +1,53 @@
 from sly import Lexer, Parser
 
+
 class MyLexer(Lexer):
-    literals = {"+"}
-    tokens = { 'NUMBER' }
-    pass
+    ignore = " \t"
+    mais = r"\+"
+    menos = r"-"
+    vezes = r"\*"
+    dividido = r"/"
+
+    tokens = {"numero", "mais", "menos", "vezes", "dividido"}
+
+    @_(r"\d+")
+    def numero(self, t):
+        t.value = int(t.value)
+        return t
+
+    def error(self, t):
+        print(f"Lexer error at '{t.value}'")
+        self.index += 1
+
 
 class MyParser(Parser):
-    debugfile = 'parser.out'
+    debugfile = "parser.out"
     tokens = MyLexer.tokens
 
     def error(self, t):
         print(f"Syntax error at '{t.value}'")
-    
-    @_('expression1','expression2')
-    def statement(self, p):
-        return p.expression1 + p.expression2
 
-    @_('expression1',"+",'expression2')
-    def expression1(self, p):
-        return p[0]
+    @_(
+        "numero mais S",
+        "numero menos S",
+        "numero vezes S",
+        "numero dividido S",
+        "numero",
+    )
+    def S(self, p):
+        print(f"Parsing: {p.numero}")
+
+        if len(p) == 1:
+            return p.numero
+        elif len(p) == 3:
+            if p[1] == "+":
+                return p.numero + p.S
+            elif p[1] == "-":
+                return p.numero - p.S
+            elif p[1] == "*":
+                return p.numero * p.S
+            elif p[1] == "/":
+                return p.numero / p.S
 
 
 parser = MyParser()
@@ -26,7 +55,4 @@ lexer = MyLexer()
 
 string_to_parse = "1 + 2 + 3"
 
-print(parser.parse(string_to_parse))
-
-
-
+print(parser.parse(lexer.tokenize(string_to_parse)))
